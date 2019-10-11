56cd29cd64aa3fed31168211ae6f6942305295ea
Lambda_School_Loaner_189 <lambda_school_loaner_189@Lambda-School-Loaner-189s-MacBook-Pro.local>
2019-10-11 09:12:26 -0700

    added projects schema

--- migrations/20191011084434_projects.js
+++ migrations/20191011084434_projects.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('projects', tbl => {
+      tbl.increments()
+      tbl.string('name', 255)
+        .notNullable()
+      tbl.string('description', 255)
+        .nullable()
+      tbl.binary('completed', 1)
+        .notNullable()
+        .defaultTo(0)
+    })
+    
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('projects')
+  };
