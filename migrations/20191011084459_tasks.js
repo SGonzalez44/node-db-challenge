4ef36729e6ce8697b92316b4aac5b3adda7f1365
Lambda_School_Loaner_189 <lambda_school_loaner_189@Lambda-School-Loaner-189s-MacBook-Pro.local>
2019-10-11 09:26:11 -0700

    added tasks and resources migration

--- migrations/20191011084459_tasks.js
+++ migrations/20191011084459_tasks.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('tasks', tbl => {
+      tbl.increments()
+      tbl.integer('task_id', 6)
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+      tbl.string('description', 255)
+        .notNullable()
+      tbl.text('notes', 500)
+        .nullable()
+      tbl.binary('completed', 1)
+        .defaultTo(0)
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('tasks')
+  };
